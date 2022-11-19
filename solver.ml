25f80653302d1b662d3f11ba410fc54161c71365
Docker <docker@example.com>
2022-11-19 14:18:01 +0000

    modified:   solver.ml

--- solver.ml
+++ solver.ml
@@ -1,4 +1,4 @@
-type available = { loc : int; possible : int list }
+type available = { loc : int * int; possible : int list }
 
 
 (* TODO: tip stanja ustrezno popravite, saj boste med reševanjem zaradi učinkovitosti
@@ -12,24 +12,49 @@ let print_state (state : state) : unit =
 
 type response = Solved of Model.solution | Unsolved of state | Fail of state
 
-let available_elements loc grid = 
+let available_elements (loc : int * int) grid = 
   let check_area f x ind = if Array.mem (Some x) (f grid ind) then false else true in
-  let row_ind = loc / 9 in
-  let col_ind = loc mod 9 in
+  let row_ind = fst loc in
+  let col_ind = snd loc in
   let box_ind = 3 * (row_ind / 3) + (col_ind / 3) in
   List.filter (
     fun num -> 
-      check_area Model.get_row num row_ind && 
-      check_area Model.get_column num col_ind && 
-      check_area Model.get_box num box_ind) 
+      check_area (Model.get_row) num row_ind && 
+      check_area (Model.get_column) num col_ind && 
+      check_area (Model.get_box) num box_ind) 
       [1;2;3;4;5;6;7;8;9] 
-let initialize_state (problem : Model.problem) : state = {
+
+let replace_element_in_grid (grid : int option Model.grid) (loc : int * int) (x : int) = 
+ let row_ind = fst loc in
+  let col_ind = snd loc in
+  let _ = grid.(row_ind).(col_ind) <- Some x in 
+  grid
+      
+let next_loc loc = 
+  match fst loc, snd loc with
+  | 8, 8 -> (8, 8)
+  | x, 8 -> ((x + 1), 0)
+  | x, y -> (x, (y+1))
+      
+let rec find_next_empty (state : state) : state = 
+  let row_ind = fst state.available.loc in
+  let col_ind = snd state.available.loc in
+  match state.current_grid.(row_ind).(col_ind) with 
+    | None -> state
+    | _ -> find_next_empty ( { current_grid = state.current_grid; 
+      problem = state.problem; 
+      available = { 
+      loc = next_loc state.available.loc; 
+      possible = available_elements (next_loc state.available.loc) state.current_grid } } )
+
+let initialize_state (problem : Model.problem) : state = 
+  {
   current_grid = Model.copy_grid problem.initial_grid; 
   problem = problem; 
   available = { 
-    loc = 0; 
-    possible = available_elements 0 problem.initial_grid }
-  }
+    loc = (0, 0); 
+    possible = available_elements (0, 0) problem.initial_grid }
+  } 
 
 let validate_state (state : state) : response =
   let unsolved =
@@ -41,23 +66,6 @@ let validate_state (state : state) : response =
     let solution = Model.map_grid Option.get state.current_grid in
     if Model.is_valid_solution state.problem solution then Solved solution
     else Fail state
-
-let replace_element_in_grid (grid : int option Model.grid) (loc : int) (x : int) = 
-  let row_ind = loc / 9 in
-  let col_ind = loc mod 9 in
-  let _ = grid.(row_ind).(col_ind) <- Some x in 
-  grid
-
-  let rec find_next_empty (state : state) : state = 
-    let row_ind = state.available.loc / 9 in
-    let col_ind = state.available.loc mod 9 in
-    match state.current_grid.(row_ind).(col_ind) with 
-    | None -> state
-    | _ -> find_next_empty ( { current_grid = Model.copy_grid state.current_grid; 
-      problem = state.problem; 
-      available = { 
-      loc = state.available.loc + 1; 
-      possible = available_elements (state.available.loc + 1) state.current_grid } } )
 
 let branch_state (state : state) : (state * state) option =
   (* TODO: Pripravite funkcijo, ki v trenutnem stanju poišče hipotezo, glede katere
@@ -72,8 +80,8 @@ let branch_state (state : state) : (state * state) option =
       { current_grid = replace_element_in_grid state'.current_grid state'.available.loc x; 
         problem = state'.problem; 
         available = { 
-        loc = state'.available.loc + 1; 
-        possible = available_elements (state'.available.loc + 1) state'.current_grid } },
+        loc = next_loc state'.available.loc; 
+        possible = available_elements (next_loc state'.available.loc) state'.current_grid } },
       { current_grid = (Model.copy_grid state'.current_grid); 
         problem = state'.problem; 
         available = { 
@@ -85,6 +93,7 @@ let branch_state (state : state) : (state * state) option =
 let rec solve_state (state : state) =
   (* uveljavimo trenutne omejitve in pogledamo, kam smo prišli *)
   (* TODO: na tej točki je stanje smiselno počistiti in zožiti možne rešitve *)
+  (* Printf.printf "ind: %i, %i " (fst state.available.loc) (snd state.available.loc); *)
   match validate_state state with
   | Solved solution ->
       (* če smo našli rešitev, končamo *)
